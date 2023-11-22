82ed32ce67a98788e9d64c85b5d42728446cd91f
suptimal <kaersten.m@web.de>
2023-11-22 07:11:43 +0100

    added lspkind, and shortend menu

--- lua/custom/plugins/autocompletion.lua
+++ lua/custom/plugins/autocompletion.lua
@@ -11,15 +11,44 @@ return {
 
     -- Adds a number of user-friendly snippets
     'rafamadriz/friendly-snippets',
+
+    -- make it look fancy
+    "onsails/lspkind.nvim",
   },
   config = function()
     -- See `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
+    local lspkind = require('lspkind')
     require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup {}
 
     cmp.setup {
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol', -- show only symbol annotations
+          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          before = function (entry, vim_item)
+            -- dont show menu inside entry
+            vim_item.menu = ""
+            return vim_item
+          end
+        }),
+      },
+        window = {
+    completion = {
+      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+      col_offset = -3,
+      side_padding = 0,
+    },
+  },
+
+      
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
